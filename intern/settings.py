04e921365f4e81107f882692f039ba495af1cb12
Aman Negi <negiaman117@gmail.com>
2021-01-01 13:50:39 +0530

    django_her

--- intern/settings.py
+++ intern/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -153,3 +154,4 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
+django_heroku.settings(locals())
