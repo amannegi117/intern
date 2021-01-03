e0be3ed59a6d610f0a9591d7bc203f6ed410c6b2
Aman Negi <negiaman117@gmail.com>
2021-01-03 22:18:08 +0530

    mediaroot

--- intern/settings.py
+++ intern/settings.py
@@ -151,11 +151,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-MEDIA_URL = 'media/'
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 django_heroku.settings(locals())
